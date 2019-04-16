5f5ba32a33e5b5506797177222542c58d5d7e1a3
Ezequiel Larrarte <ezequiel.larrarte@despegar.com>
2019-04-16 14:05:22 -0300

    -

---
+++ exec/testing-flask/testing-flask.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+# INFORMACION DEL SCRIPT
+# ---------------------------------------------------------
+# AUTOR   : Ezequiel Larrarte
+# FECHA   : 16-04-2019
+# VERSION : 1
+#
+# DESCRIPCION
+# Pruebas con el framework [Flask]
+# ---------------------------------------------------------
+
+# LIBRERIAS
+# ---------------------------------------------------------
+import os
+import sys
+sys.path.append(os.environ['SCRIPTS'] + '/include')
+import main
+from flask import Flask
+from flask import render_template
+# ---------------------------------------------------------
+
+# CONFIGURACION
+# ---------------------------------------------------------
+# ---------------------------------------------------------
+
+# FUNCIONES
+# ---------------------------------------------------------
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return('Testing Flask!')
+
+@app.route("/template/<name>")
+def template(name=None):
+    return(render_template('template.html', name=name))
+
+@app.route("/signature")
+def signature():
+    return(render_template('signature.html'))
+# ---------------------------------------------------------
+
+# PRINCIPAL
+# ---------------------------------------------------------
+# ---------------------------------------------------------
+
+
