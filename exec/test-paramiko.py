d1d815f6b3626b24356403a9911a923f55a05b67
Ezequiel Larrarte <ezequiel.larrarte@despegar.com>
2020-04-25 16:22:21 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   exec/test-paramiko.py
    
     Untracked files:
    	.vscode/

--- exec/test-paramiko.py
+++ exec/test-paramiko.py
@@ -2,17 +2,18 @@
 
 import paramiko
 from importlib import import_module
-f_fn = import_module("fn1", package="f_fn")
+#f_fn = import_module("fn1", package="f_fn")
 
-t_host="sec-test-00"
-t_username="ezequiel.larrarte"
+t_host="oficina.estudioacoppo.com.ar"
+n_port=10000
+t_username="root"
 
 #
 ssh_client = paramiko.SSHClient()
 ssh_client.load_system_host_keys()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(t_host, username=t_username)
-f_fn(ssh_client)
+ssh_client.connect(t_host, username=t_username, port=n_port)
+#f_fn(ssh_client)
 ssh_client.close()
 #
 
