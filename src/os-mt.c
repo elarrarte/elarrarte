2ad7ef0e08530564fcdef40bd0384905b8b89a02
Ezequiel Larrarte <elarrarte@gmail.com>
2021-05-02 11:56:08 -0300

    -

--- src/os-mt.c
+++ src/os-mt.c
@@ -22,6 +22,7 @@
 #define MAX_INSTRUCTIONS                                    3
 #define MAX_INSTRUCTION_SIZE                                5
 
+#define CPU_UNDEFINED                                       -1
 #define PROCESS_SLEEP                                       1
 #define PROCESS_INSTRUCTION_END                             1
 #define PROCESS_QUANTUM_END                                 2
@@ -30,6 +31,7 @@
 
 #define INITIALIZATION_SLEEP                                500000
 #define VM_SLEEP                                            1
+#define DEBUG_SLEEP                                         100000
 #define CPU_THREAD_SLEEP                                    1
 #define IO_THREAD_SLEEP                                     1
 #define MAX_THREAD_NAME                                     255
@@ -66,6 +68,7 @@ typedef struct
     int execution_time;
     int cpu_time;
     int io_time;
+    int remaining_instructions;
 
 } process_stats_t;
 
@@ -80,6 +83,7 @@ typedef struct pcb
     int instructions;
     process_state_t state;
     process_stats_t stats;
+    int cpu_id;
 
 } pcb_t;
 
@@ -94,9 +98,10 @@ typedef struct
 #define CPU_CORES                                           4
 list_t *cpu_queue, *io_queues[IO_DEVICES_SIZE], *process_queues[PROCESS_STATES_SIZE];
 window_list_t windows[IO_DEVICES_SIZE + PROCESS_STATES_SIZE + 1];
-window_t *process_window, *log_window;
-pthread_t cpu_threads[CPU_CORES], io_threads[IO_DEVICES_SIZE];
-pthread_mutex_t mutex;
+window_t *process_window, *log_window, *debug_window;
+pthread_t cpu_threads[CPU_CORES], io_threads[IO_DEVICES_SIZE], debug_thread;
+pthread_mutex_t mutex, mutex_gui;
+pcb_t *debug_pcb = NULL;
 
 pcb_t *create_pcb(pid_t pid)
 {
@@ -105,21 +110,21 @@ pcb_t *create_pcb(pid_t pid)
     int index;
 
     pcb = (pcb_t *) malloc (sizeof(pcb_t));
-    //pcb->priority = HIGH;
+    //pcb->priority = LOW;
     pcb->priority = rand() % PROCESS_PRIORITIES_SIZE;
     pcb->pid = pid;
     pcb->uid = rand() % MAX_UID;
     pcb->gid = rand() % MAX_GID;
     memset(&pcb->stats, 0, sizeof(process_stats_t));
-    //pcb->instructions = 1;
-    pcb->instructions = (rand() % MAX_INSTRUCTIONS) + 1;
+    pcb->instructions = (rand() % MAX_INSTRUCTIONS) + 3;
+    pcb->stats.remaining_instructions = pcb->instructions;
     pcb->program = (instruction_t *) malloc(sizeof(instruction_t) * pcb->instructions);
+    pcb->cpu_id = CPU_UNDEFINED;
     instruction = pcb->program;
     for(index = 0; index < pcb->instructions; index++)
     {
-        //instruction->type = IO;
+        //instruction->type = CPU;
         instruction->type = rand() % INSTRUCTION_TYPES_SIZE;
-        //instruction->size = 10;
         instruction->size = (rand() % MAX_INSTRUCTION_SIZE) + 1;
         if(instruction->type == IO)
         {
@@ -159,39 +164,40 @@ pcb_t *create_process(void)
     }
 }
 
-void print_pcb(pcb_t *pcb)
+void print_pcb(pcb_t *pcb, window_t *window)
 {
     int index;
     instruction_t *instruction;
 
-    wclear(process_window->user_window);
-    wprintw(process_window->user_window, "PCB\n");
-    wprintw(process_window->user_window, "[GENERAL]\n");
-    wprintw(process_window->user_window, "PRIORITY             : %s\n", PROCESS_PRIORITIES[pcb->priority]);
-    wprintw(process_window->user_window, "PID                  : %d\n", pcb->pid);
-    wprintw(process_window->user_window, "UID                  : %d\n", pcb->uid);
-    wprintw(process_window->user_window, "GID                  : %d\n", pcb->gid);
-    wprintw(process_window->user_window, "STATE                : %s\n", PROCESS_STATES[pcb->state]);
-    wprintw(process_window->user_window, "\n[STATS]\n");
-    wprintw(process_window->user_window, "EXECUTION TIME       : %d\n", pcb->stats.execution_time);
-    wprintw(process_window->user_window, "CPU TIME             : %d\n", pcb->stats.cpu_time);
-    wprintw(process_window->user_window, "IO TIME              : %d\n", pcb->stats.io_time);
-    wprintw(process_window->user_window, "\n[PROGRAM]\n");
-    wprintw(process_window->user_window, "INSTRUCTIONS         : %d\n", pcb->instructions);
+    pthread_mutex_lock(&mutex_gui);
+    wclear(window->user_window);
+    wprintw(window->user_window, "PCB\n");
+    wprintw(window->user_window, "[GENERAL]\n");
+    wprintw(window->user_window, "PRIORITY               : %s\n", PROCESS_PRIORITIES[pcb->priority]);
+    wprintw(window->user_window, "PID                    : %d\n", pcb->pid);
+    wprintw(window->user_window, "UID                    : %d\n", pcb->uid);
+    wprintw(window->user_window, "GID                    : %d\n", pcb->gid);
+    wprintw(window->user_window, "STATE                  : %s\n", PROCESS_STATES[pcb->state]);
+    wprintw(window->user_window, "CPUID                  : %d\n", pcb->cpu_id);
+    wprintw(window->user_window, "\n[STATS]\n");
+    wprintw(window->user_window, "EXECUTION TIME         : %d\n", pcb->stats.execution_time);
+    wprintw(window->user_window, "CPU TIME               : %d\n", pcb->stats.cpu_time);
+    wprintw(window->user_window, "IO TIME                : %d\n", pcb->stats.io_time);
+    wprintw(window->user_window, "REMAINING INSTRUCTIONS : %d\n", pcb->stats.remaining_instructions);
+    wprintw(window->user_window, "\n[PROGRAM]\n");
+    wprintw(window->user_window, "INSTRUCTIONS           : %d\n", pcb->instructions);
     instruction = pcb->program;
     for(index = 0; index < pcb->instructions; index++)
     {
-        wprintw(process_window->user_window, "INSTRUCION[%d] TYPE   : %s\n", index, INSTRUCTION_TYPES[instruction->type]);
-        wprintw(process_window->user_window, "INSTRUCION[%d] SIZE   : %d\n", index, instruction->size);
+        wprintw(window->user_window, "INSTRUCION[%d] TYPE     : %s\n", index, INSTRUCTION_TYPES[instruction->type]);
         if(instruction->type == IO)
-        {
-            wprintw(process_window->user_window, "INSTRUCION[%d] DEVICE : %s\n", index, IO_DEVICES[instruction->device]);
-        }
+            wprintw(window->user_window, "INSTRUCION[%d] DEVICE   : %s\n", index, IO_DEVICES[instruction->device]);
+        wprintw(window->user_window, "INSTRUCION[%d] SIZE     : %d\n", index, instruction->size);
         instruction++;
     }
-    wrefresh(process_window->user_window);
+    wrefresh(window->user_window);
+    pthread_mutex_unlock(&mutex_gui);
 }
-
 
 void print_queues()
 {
@@ -200,6 +206,7 @@ void print_queues()
     int index;
 
     pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex_gui);
     for(index = 0; index < PROCESS_STATES_SIZE + IO_DEVICES_SIZE + 1; index++)
     {
         wclear(windows[index].window->user_window);
@@ -213,6 +220,7 @@ void print_queues()
         wnoutrefresh(windows[index].window->user_window);
     }
     doupdate();
+    pthread_mutex_unlock(&mutex_gui);
     pthread_mutex_unlock(&mutex);
 }
 
@@ -228,7 +236,7 @@ void finish_pcb(pcb_t *pcb)
     list_remove_data(process_queues[RUNNING], pcb);
     list_remove_data(process_queues[ACTIVE], pcb);
     list_add_tail(process_queues[FINISHED], pcb);
-    print_pcb(pcb);
+    print_pcb(pcb, process_window);
     pthread_mutex_unlock(&mutex);
     wprintw(log_window->user_window, "PROCESS [%d] finished with status [%d]\n", pcb->pid, child_status);
     wrefresh(log_window->user_window);
@@ -270,10 +278,10 @@ void handle_io_instrucion(pcb_t *pcb)
     }
     else
     {
-        pcb->instructions--;
+        pcb->stats.remaining_instructions--;
         pcb->ip++;
 
-        if(pcb->instructions)
+        if(pcb->stats.remaining_instructions)
         {
             pthread_mutex_lock(&mutex);
             list_remove_data(process_queues[RUNNING], pcb);
@@ -293,7 +301,7 @@ int process_cpu_instruction(pcb_t *pcb)
 {
     int quantum = QUANTUM_BASE * (pcb->priority + 1);
 
-    while(pcb->instructions)
+    while(pcb->stats.remaining_instructions)
     {
         while(pcb->ip->size)
         {
@@ -310,7 +318,7 @@ int process_cpu_instruction(pcb_t *pcb)
             }
         }
 
-        pcb->instructions--;
+        pcb->stats.remaining_instructions--;
         pcb->ip++;
     }
 
@@ -336,10 +344,10 @@ void handle_cpu_instruction(pcb_t *pcb)
     }
 }
 
-void *cpu_thread(void *vp_cpuid)
+void *cpu_thread_function(void *vp_cpu_id)
 {
     pcb_t* pcb;
-    int cpuid = *((int *) vp_cpuid);
+    int cpu_id = *((int *) vp_cpu_id);
 
     while(TRUE)
     {
@@ -349,8 +357,9 @@ void *cpu_thread(void *vp_cpuid)
             pcb = list_remove_head(process_queues[READY]);
             list_add_tail(process_queues[RUNNING], pcb);
             list_add_tail(cpu_queue, pcb);
-            pcb->state = RUNNING;
             pthread_mutex_unlock(&mutex);
+            pcb->state = RUNNING;
+            pcb->cpu_id = cpu_id;
 
             switch(pcb->ip->type)
             {
@@ -362,13 +371,19 @@ void *cpu_thread(void *vp_cpuid)
                     handle_io_instrucion(pcb);
                 break;
             }
+
+            pcb->cpu_id = CPU_UNDEFINED;
+            sleep(CPU_THREAD_SLEEP);
         }
-        pthread_mutex_unlock(&mutex);
+        else
+        {
+            pthread_mutex_unlock(&mutex);
+        }
         sleep(CPU_THREAD_SLEEP);
     }
 }
 
-void *io_thread(void *vp_device)
+void *io_thread_function(void *vp_device)
 {
     int device = *((int *) vp_device);
     pcb_t *pcb;
@@ -405,6 +420,17 @@ void *io_thread(void *vp_device)
     }
 }
 
+void *debug_thread_function()
+{
+    while(debug_pcb == NULL) sleep(VM_SLEEP);
+
+    while(TRUE)
+    {
+        print_pcb(debug_pcb, debug_window);
+        usleep(DEBUG_SLEEP);
+    }
+}
+
 void initilize_gui()
 {
     int index, window_index = 0;
@@ -418,10 +444,15 @@ void initilize_gui()
     window_title(process_window, "PROCESS");
     wnoutrefresh(process_window->border_window);
 
-    log_window = window_create(WINDOW_HEIGHT * 3, WINDOW_WIDTH * 10, WINDOW_HEIGHT, WINDOW_WIDTH * 4);
+    log_window = window_create(WINDOW_HEIGHT * 3, WINDOW_WIDTH * 6, WINDOW_HEIGHT, WINDOW_WIDTH * 4);
     scrollok(log_window->user_window, TRUE);
     window_title(log_window, "LOG");
     wnoutrefresh(log_window->border_window);
+
+    debug_window = window_create(WINDOW_HEIGHT * 3, WINDOW_WIDTH * 4, WINDOW_HEIGHT, WINDOW_WIDTH * 10);
+    window_title(debug_window, "DEBUG");
+    wnoutrefresh(debug_window->border_window);
+
 
     for(index = 0; index < PROCESS_STATES_SIZE; index++)
     {
@@ -474,7 +505,7 @@ void initialize_resources()
         window_index++;
         device = (int *) malloc(sizeof(int));
         *device = index;
-        exitcode = pthread_create(&io_threads[index], NULL, io_thread, (void *) device);
+        exitcode = pthread_create(&io_threads[index], NULL, io_thread_function, (void *) device);
         if(exitcode == EXIT_SUCCESS)
         {
             sprintf(thread_name, "io-t-%s", IO_DEVICES[index]);            
@@ -496,7 +527,7 @@ void initialize_resources()
     {
         cpuid = (int *) malloc(sizeof(int));
         *cpuid = index;
-        exitcode = pthread_create(&cpu_threads[index], NULL, cpu_thread, (void *) cpuid);
+        exitcode = pthread_create(&cpu_threads[index], NULL, cpu_thread_function, (void *) cpuid);
         if(exitcode == EXIT_SUCCESS)
         {
             sprintf(thread_name, "cpu-t-%d", index);
@@ -511,6 +542,8 @@ void initialize_resources()
     usleep(INITIALIZATION_SLEEP);
     wprintw(log_window->user_window, "* CPU queue and threads\n");
     wrefresh(log_window->user_window);
+
+    pthread_create(&debug_thread, NULL, debug_thread_function, NULL);
 }
 
 void create_processes()
@@ -528,13 +561,14 @@ void create_processes()
     {
         pcb = create_process();
         pcb->state = NEW;
+        if(debug_pcb == NULL) debug_pcb = pcb;
         pthread_mutex_lock(&mutex);
         list_add_tail(process_queues[NEW], pcb);
         pthread_mutex_unlock(&mutex);
         usleep(500000);
         wprintw(log_window->user_window, "* PROCESS [%d]\n", pcb->pid);
         wrefresh(log_window->user_window);
-        print_pcb(pcb);
+        print_pcb(pcb, process_window);
     }
 }
 
